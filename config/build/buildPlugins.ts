ff12a0176d6099ab47ab87d2ef30d632e1e00bf0
Ksenia <baranova-ksenia1990@gmail.com>
2023-10-23 19:59:05 +0300

    setup i18n

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,22 +1,25 @@
-import path from "path";
-import webpack from "webpack";
-import HTMLWebpackPlugin from "html-webpack-plugin";
-import { BuildOptions } from "./types/config";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import HTMLWebpackPlugin from 'html-webpack-plugin'
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import webpack from 'webpack'
+import { BuildOptions } from './types/config'
 
 export function buildPlugins({
-  paths,
+	paths,
+	isDev,
 }: BuildOptions): webpack.WebpackPluginInstance[] {
-  return [
-    new HTMLWebpackPlugin({
-      // в объекте передаем настройки плагина
-      // index.html - шаблон, который будет использоваться для сборки
-      template: paths.html,
-    }),
-    new webpack.ProgressPlugin(),
-    new MiniCssExtractPlugin({
-      filename: "css/[name].[contenthash:8].css",
-      chunkFilename: "css/[name].[contenthash:8].chunk.css",
-    }),
-  ];
+	return [
+		new HTMLWebpackPlugin({
+			// в объекте передаем настройки плагина
+			// index.html - шаблон, который будет использоваться для сборки
+			template: paths.html,
+		}),
+		new webpack.ProgressPlugin(),
+		new MiniCssExtractPlugin({
+			filename: 'css/[name].[contenthash:8].css',
+			chunkFilename: 'css/[name].[contenthash:8].chunk.css',
+		}),
+		new webpack.DefinePlugin({
+			__IS_DEV__: JSON.stringify(isDev),
+		}),
+	]
 }
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,6 @@
 import { useTheme } from 'app/providers/ThemeProvider/lib/useTheme'
 import { AppRouter } from 'app/providers/router'
+import { Suspense } from 'react'
 import { classNames } from 'shared/lib/classNames/classNames'
 import { Navbar } from 'widgets/Navbar'
 import { Sidebar } from 'widgets/Sidebar'
@@ -10,11 +11,14 @@ const App = () => {
 
 	return (
 		<div className={classNames('app', {}, [theme])}>
-			<Navbar />
-			<div className='content-page'>
-				<Sidebar />
-				<AppRouter />
-			</div>
+			<Suspense fallback=''>
+				<Navbar />
+
+				<div className='content-page'>
+					<Sidebar />
+					<AppRouter />
+				</div>
+			</Suspense>
 		</div>
 	)
 }
