0d2b650688590badef0c7068c0d9a657663a725b
Ksenia <baranova-ksenia1990@gmail.com>
2023-10-25 13:45:19 +0300

    add notfoundpage, page loader

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,5 +1,6 @@
 import { AboutPage } from 'pages/AboutPage';
 import { MainPage } from 'pages/MainPage';
+import { NotFoundPage } from 'pages/NotFoundPage';
 import React from 'react';
 import { RouteProps } from 'react-router-dom';
 
@@ -7,12 +8,14 @@ import { RouteProps } from 'react-router-dom';
 export enum AppRoutes {
 MAIN = 'main',
 ABOUT = 'about',
+NOT_FOUND = 'not_found',
 }
 
 // Define the route paths for each route name
 export const RoutePath: Record<AppRoutes, string> = {
     [AppRoutes.MAIN]: '/',
     [AppRoutes.ABOUT]: '/about',
+    [AppRoutes.NOT_FOUND]: '*',
 };
 
 // Define the components for each route name.
@@ -20,6 +23,7 @@ export const RouteComponent:
 Record<AppRoutes, React.ReactNode> = {
     [AppRoutes.MAIN]: <MainPage />,
     [AppRoutes.ABOUT]: <AboutPage />,
+    [AppRoutes.NOT_FOUND]: <NotFoundPage />,
 };
 
 // Define the route configuration by combining route paths and components.
@@ -31,5 +35,9 @@ export const routeConfig: Record<AppRoutes, RouteProps> = {
     [AppRoutes.ABOUT]: {
         path: RoutePath[AppRoutes.ABOUT],
         element: RouteComponent[AppRoutes.ABOUT],
+    },
+    [AppRoutes.NOT_FOUND]: {
+        path: RoutePath[AppRoutes.NOT_FOUND],
+        element: RouteComponent[AppRoutes.NOT_FOUND],
     },
 };
