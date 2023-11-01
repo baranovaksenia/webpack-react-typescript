2243668fb32156c59a9c9c0aa007fe0f26e0c645
Ksenia <baranova-ksenia1990@gmail.com>
2023-11-01 17:09:37 +0200

    add stories for components and pages

---
+++ src/pages/MainPage/ui/MainPage.stories.tsx
@@ -0,0 +1,21 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { Theme } from 'app/providers/ThemeProvider';
+import { MainPage } from 'pages/MainPage';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+
+export default {
+    title: 'pages/NotFoundPage',
+    component: MainPage,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as ComponentMeta<typeof MainPage>;
+
+const Template: ComponentStory<typeof MainPage> = (args) => <MainPage />;
+
+export const Normal = Template.bind({});
+Normal.args = {};
+
+export const Dark = Template.bind({});
+Dark.args = {};
+Dark.decorators = [ThemeDecorator(Theme.DARK)];
